e2b8fe5d43a62964239a1c34d0a85cf36d230bfd
Michael Kimberlin <mkimberlin@gmail.com>
2014-09-16 16:37:52 -0500

    Update basketball site for 2014-15

---
+++ basketball/js/menu.js
@@ -0,0 +1,37 @@
+function hideCurrentlyActive() {
+    'use strict';
+    var wasActiveItem = $('.navbar-nav .active'), toHide = $('div.container.active');
+    wasActiveItem.removeClass('active');
+
+    toHide.removeClass('active');
+    toHide.addClass('hidden');
+}
+
+function showDiv(toShow) {
+    'use strict';
+    toShow.removeClass('hidden');
+    toShow.addClass('active');
+}
+
+// Start with the appropriate anchor...failure to do this will result in onanchorchange not being called when
+// going "back" to the anchor-less start page.
+var hash = window.location.hash;
+if (hash === '') {
+    window.location.hash = "#home";
+}
+
+window.onanchorchange = function () {
+    'use strict';
+    hideCurrentlyActive();
+
+    //Activate appropriate menu item
+    var hash = window.location.hash;
+    if (hash === '') {
+        window.location.hash = "#home";
+    }
+
+    $('.navbar-nav li').has('a[href="' + hash + '"]').addClass('active');
+    showDiv($(hash));
+};
+
+window.onanchorchange();
